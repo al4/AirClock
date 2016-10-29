60bb662d6aa5ff537cd3da68f94915bef3206d44
Alex Forbes <github@al4.co.nz>
2016-10-30 00:02:29 +0100

    Add gpl license

--- app/src/main/java/nz/al4/airclock/AirClockApp.java
+++ app/src/main/java/nz/al4/airclock/AirClockApp.java
@@ -5,7 +5,22 @@ import android.app.Application;
 import net.danlew.android.joda.JodaTimeAndroid;
 
 /**
- * Created by alex on 23/10/2016.
+ Copyright 2016 Alex Forbes
+
+ This file is part of AirClock.
+
+ AirClock is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ AirClock is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with AirClock.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 public class AirClockApp extends Application {
