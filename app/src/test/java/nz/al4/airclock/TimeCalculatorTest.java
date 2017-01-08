9d7fd74184fcc249123773556e8c8027c2630f27
Alex Forbes <github@al4.co.nz>
2017-01-08 14:55:29 +0000

    Add unit test skeleton for time zone functions

---
+++ app/src/test/java/nz/al4/airclock/TimeCalculatorTest.java
@@ -0,0 +1,44 @@
+package nz.al4.airclock;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by alex on 07/01/2017.
+ */
+public class TimeCalculatorTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getFlightLength() throws Exception {
+        assertEquals(true, true);
+
+    }
+
+//    @Test
+//    public void getElapsed() throws Exception {
+//
+//    }
+//
+//    @Test
+//    public void getTotalTimeShift() throws Exception {
+//
+//    }
+//
+//    @Test
+//    public void getEffectiveOffset() throws Exception {
+//
+//    }
+
+}
